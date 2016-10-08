bf50c5cc7f9fc868741d5343793ca39c207a4d3f
Ajusa Jain <arhamjain@gmail.com>
2016-10-08 16:35:39 -0400

    Added vuejs frontend stuff
    does NOT consume api yet
    that is next

---
+++ public/assets/js/main.js
@@ -0,0 +1,29 @@
+new Vue({
+    el: 'body',
+    data: {
+        currentPage: 'home',
+        currentSpeed: '50',
+        inCarTemp: '10',
+        outCarTemp: '9',
+        chargePerMile: '270'
+    },
+    methods: {
+        cancelTrip: function() {
+            swal({
+                title: 'Are you sure?',
+                text: 'You wont be able to revert this!',
+                type: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Stop the trip!'
+            }).then(function() {
+                swal(
+                    'Cancelled!',
+                    'Your trip has been cancelled.',
+                    'success'
+                );
+            })
+        },
+    },
+})
