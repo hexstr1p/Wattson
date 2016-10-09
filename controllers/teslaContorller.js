b5f63eb66f596213c7aaac53ad8467cc56843e67
lunete <claud@openmailbox.org>
2016-10-08 21:06:36 -0400

    fixed urls

--- controllers/teslaContorller.js
+++ controllers/teslaContorller.js
@@ -16,58 +16,51 @@ module.exports = function(app) {
     });
   };
 
+  var gloablURL = 'https://private-anon-81aa2b52ca-timdorr.apiary-mock.com';
+  var protoURL = gloablURL + '/api/1/vehicles/';
+
     // authentications
-  var authenicate = makeCall('https://private-anon-81aa2b52ca-timdorr.apiary-mock.com/oauth/token');
+  var authenicate = makeCall(gloablURL + '/oauth/token');
   var wakeCar = function(vehicleID) {
-    makeCall('https://owner-api.teslamotors.com/api/1/vehicles/' + vehicleID +
-      '/wake_up');
+    makeCall(protoURL + vehicleID + '/wake_up');
   };
   var setValet = function(vehicleID, on, passwd) {
-    makeCall('https://owner-api.teslamotors.com/api/1/vehicles/' + vehicleID +
-      '/command/set_valet_mode');
+    makeCall(gloablURL + vehicleID + '/command/set_valet_mode');
   };
   var resetValetPin = function(vehicleID) {
-    makeCall('https://owner-api.teslamotors.com/api/1/vehicles/' + vehicleID +
-      '/command/reset_valet_pin');
+    makeCall(protoURL + vehicleID + '/command/reset_valet_pin');
   };
   var flashLights = function(vehicleID) {
-    makeCall('https://owner-api.teslamotors.com/api/1/vehicles/' + vehicleID +
-      '/command/flash_lights');
+    makeCall(protoURL + vehicleID + '/command/flash_lights');
   };
   var honkHorn = function(vehicleID) {
-    makeCall('https://owner-api.teslamotors.com/api/1/vehicles/' + vehicleID +
-      '/command/flash_lights');
+    makeCall(protoURL + vehicleID + '/command/flash_lights');
   };
   var unlockDoors = function(vehicleID) {
-    makeCall('https://owner-api.teslamotors.com/api/1/vehicles/' + vehicleID +
-      '/command/door_unlock');
+    makeCall(protoURL + vehicleID + '/command/door_unlock');
   };
   var lockDoors = function(vehicleID) {
-    makeCall('https://owner-api.teslamotors.com/api/1/vehicles/' + vehicleID +
-      '/command/door_lock');
+    makeCall(protoURL + vehicleID + '/command/door_lock');
   };
   var setTemp = function(vehicleID, temp) {
-    makeCall('https://owner-api.teslamotors.com/api/1/vehicles/' + vehicleID +
-      '/command/set_temps?driver_temp=' + temp + '&passenger_temp=' + temp);
+    makeCall(protoURL + vehicleID  + '/command/set_temps?driver_temp=' +
+    temp + '&passenger_temp=' + temp);
   };
   var startHVAC = function(vehicleID) {
-    makeCall('https://owner-api.teslamotors.com/api/1/vehicles/' + vehicleID +
-      '/command/auto_conditioning_start');
+    makeCall(protoURL + vehicleID + '/command/auto_conditioning_start');
   };
   var endHVAC = function(vehicleID) {
-    makeCall('https://owner-api.teslamotors.com/api/1/vehicles/' + vehicleID +
-      '/command/auto_conditioning_stop');
+    makeCall(protoURL + vehicleID + '/command/auto_conditioning_stop');
   };
   var adjustRoof = function(vehicleID, state, percent) {
-    makeCall('https://owner-api.teslamotors.com/api/1/vehicles/' + vehicleID +
-      '/command/sun_roof_control?state=' + state + '&percent=' + percent);
+    makeCall(protoURL + vehicleID + '/command/sun_roof_control?state=' +
+    state + '&percent=' + percent);
   };
 
   var getTemp = function(vehicleID) {
     request({
       method: 'GET',
-      url: 'https://owner-api.teslamotors.com/api/1/vehicles/' + vehicleID +
-        '/data_request/climate_state',
+      url: protoURL + vehicleID + '/data_request/climate_state',
       headers: {
         'Authorization': 'Bearer {access_token}'
       }
@@ -82,7 +75,7 @@ module.exports = function(app) {
   var getDriveAndLocation = function(vehicleID) {
     request({
       method: 'GET',
-      url: 'https://owner-api.teslamotors.com/api/1/vehicles/' + vehicleID + '/data_request/drive_state',
+      url: protoURL + vehicleID + '/data_request/drive_state',
       headers: {
         'Authorization': 'Bearer {access_token}'
       }
@@ -94,4 +87,4 @@ module.exports = function(app) {
       console.log('Response:', body);
     });
   };
-}
+};
